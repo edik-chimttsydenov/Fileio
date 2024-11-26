b07306c260c04709c3781eda5a2d06a7059a2801
edik-Chimittsydenov <edik.chimittsydenov@gmail.com>
2024-11-26 21:32:03 +0300

    Added auto coping link to clipboard

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import filedialog as fd
 from tkinter import ttk
 from tkinter import messagebox as mb
+import pyperclip
 import requests
 
 
@@ -16,6 +17,8 @@ def upload():
                 link = response.json()['link']
                 entry.delete(0, END)
                 entry.insert(0, link)
+                pyperclip.copy(link)
+                mb.showinfo("Ссылка скопирована",f"Ссылка {link}")
     except Exception as e:
         mb.showerror("Ошибка",f"Произошла ошибка {e}")
 
